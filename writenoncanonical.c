43c9086f80fa811e00e3f42663b8c1b21a53bfa1
BVFernandes <up200707284@g.uporto.pt>
2018-10-11 12:33:45 +0100

    code organizing

--- writenoncanonical.c
+++ writenoncanonical.c
@@ -21,6 +21,82 @@
 
 volatile int STOP=FALSE;
 
+void printBuff(char *buff, int size){
+	printf("\nPrinting buffer: ");
+    int i = 0;
+      for (i=0; i<size;i++){
+        printf("%02x ", buff[i]);
+     }
+	printf("\n");
+}
+
+int sendSET(){
+	bzero(bufw,255);
+
+	bufw[0] = FLAG;
+	bufw[1] = A;
+	bufw[2] = SET;
+	bufw[3] = A ^ SET;
+	bufw[4] = FLAG;
+
+	size_t length  = sizeof(bufw);
+
+    	res = write(fd,bufw,length);
+	printBuff(bufw, length);
+   	printf("%d bytes written\n", res);
+
+	if(res!=5)
+		return 1;
+	else
+		return 0;
+}
+
+int receiveACK(){
+while (STOP == FALSE) {
+		res = read(fd, &l, 1);
+		switch(state) {
+			case -1:
+				bzero(bufw, sizeof(bufw));
+				state = 0;
+				break;
+    		case 0:
+				if (l == FLAG) { state = 1; bufw[0] = l; }
+				else state = -1;
+				break;
+    		case 1:
+				if (l == A) { state = 2; bufw[1] = l; }
+				else if (l == FLAG) state = 1;
+				else state = -1;
+				break;
+    		case 2:
+				if (l == UA) { state = 3; bufw[2] = l; }
+				else if (l == FLAG) state = 1;
+				else state = -1;
+				break;
+    		case 3:
+				if (l == A ^ UA) { state = 4; bufw[3] = l; }
+				else if (l == FLAG) state = 1;
+				else state = -1;
+				break;
+    		case 4:
+				if (l == FLAG) { state = 5; bufw[4] = l; }
+				else state = -1;
+				break;
+			case 5:
+				STOP = TRUE;
+				printf("ACK received.\n");
+				break;
+		}
+	}
+
+	printBuff(bufw, 5);
+	return 0;
+}
+
+int llopen(){
+	return (sendSET() || receiveACK());
+}
+
 int main(int argc, char** argv)
 {
     int fd,c, res;
@@ -64,13 +140,7 @@ int main(int argc, char** argv)
     newtio.c_cc[VTIME]    = 0;   /* inter-character timer unused */
     newtio.c_cc[VMIN]     = 1;   /* blocking read until 1 chars received */
 
-
-
-  /* 
-    VTIME e VMIN devem ser alterados de forma a proteger com um temporizador a 
-    leitura do(s) pr�ximo(s) caracter(es)
-  */
-
+	
 
 
     tcflush(fd, TCIOFLUSH);
@@ -81,96 +151,15 @@ int main(int argc, char** argv)
     }
 
     printf("New termios structure set\n");
-
-	bzero(bufw,255);
-	/*gets(bufw);
-
-    for (i = 0; i < 255; i++) {
-      buf[i] = 'a';
-    }
-    
-    //testing
-    buf[25] = '\n';
-    */
-
-	bufw[0] = FLAG;
-	bufw[1] = A;
-	bufw[2] = SET;
-	bufw[3] = A ^ SET;
-	bufw[4] = FLAG;
-
-	size_t length  = sizeof(bufw);
-
-    printf("oi\n");
-    printf("%d\n",bufw[0]);
-    printf("%d\n",bufw[1]);
-    printf("%d\n",bufw[2]);
-    printf("%d\n",bufw[3]);
-    printf("%d\n",bufw[4]);
-
-    res = write(fd,bufw,length);   
-    printf("%d bytes written\n", res);
  
-
-  /* 
-    O ciclo FOR e as instru��es seguintes devem ser alterados de modo a respeitar 
-    o indicado no gui�o 
-  */
-
-while (STOP == FALSE) {
-		res = read(fd, &l, 1);
-		switch(state) {
-			case -1:
-				bzero(bufw, sizeof(bufw));
-				state = 0;
-				break;
-    		case 0:
-				if (l == FLAG) { state = 1; bufw[0] = l; }
-				else state = -1;
-				break;
-    		case 1:
-				if (l == A) { state = 2; bufw[1] = l; }
-				else if (l == FLAG) state = 1;
-				else state = -1;
-				break;
-    		case 2:
-				if (l == UA) { state = 3; bufw[2] = l; }
-				else if (l == FLAG) state = 1;
-				else state = -1;
-				break;
-    		case 3:
-				if (l == A ^ UA) { state = 4; bufw[3] = l; }
-				else if (l == FLAG) state = 1;
-				else state = -1;
-				break;
-    		case 4:
-				if (l == FLAG) { state = 5; bufw[4] = l; }
-				else state = -1;
-				break;
-			case 5:
-				STOP = TRUE;
-				break;
-		}
-    	printf("%s\n", bufw);
-	}
-
-	/*printf("li:\n");
-	while (STOP==FALSE) {       
-      res = read(fd,bufr,1);  
-      bufr[res]=0;               
-      printf(":%s:%d\n", bufr, res);
-     	 if (bufr[0]=='\0') STOP=TRUE;
-    }*/
+	llopen();
 
 	sleep(3);
-
    
     if ( tcsetattr(fd,TCSANOW,&oldtio) == -1) {
       perror("tcsetattr");
       exit(-1);
     }
-
-
 
 
     close(fd);
